53cffa6300f78a9f77769bc959c052f4c552bae3
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 21:20:59 +0100

    add: initializing pygame

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}, Screen width: {SCREEN_WIDTH} \n Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
 
 if __name__ == "__main__":
     main()
