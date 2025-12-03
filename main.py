f1733caa77cd47e5ec3f54cea5377fc2c152ff7a
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 21:33:21 +0100

    add: creating black game window for asteroids

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
