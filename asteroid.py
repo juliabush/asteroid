d403c69598628cdcdabd6bce4c3fc5b371073ac7
Julia Bush <j.elizabethbush@gmail.com>
2025-12-04 17:40:15 +0100

    add: creating a update method with velocity and dt parameters

--- asteroid.py
+++ asteroid.py
@@ -2,10 +2,14 @@ from constants import LINE_WIDTH
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        super()__init__(velocity)
         self.x = x
         self.y = y
         self.radius = radius
 
+
     def draw(screen, "white", (0,0), radius, LINE_WIDTH):
         pygame.draw.circle(screen, "white", (0,0), radius, LINE_WIDTH)
-    def update():
+    def update(self.velocity, dt):
+        position += self.velocity * dt
+        
