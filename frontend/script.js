119bbd1ea6271a729c7d5a684e8aca8d9d0fd774
Julia Bush <j.elizabethbush@gmail.com>
2026-03-11 15:54:38 +0100

    add: adjusting the rotation of the flame behind the ship

--- frontend/script.js
+++ frontend/script.js
@@ -353,7 +353,7 @@ function render() {
         );
 
         ctx.save();
-        ctx.rotate(Math.PI);
+        ctx.rotate(Math.PI - (45 * Math.PI) / 180);
         ctx.translate(0, 10);
 
         ctx.fillStyle = "orange";
