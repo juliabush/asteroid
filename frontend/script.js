d8048c3972b9818f3a140f2b269daa030c2e3ca1
Julia Bush <j.elizabethbush@gmail.com>
2026-03-11 16:00:37 +0100

    add: diff number for flame

--- frontend/script.js
+++ frontend/script.js
@@ -353,7 +353,7 @@ function render() {
         );
 
         ctx.save();
-        ctx.rotate(Math.PI + 55);
+        ctx.rotate(Math.PI + 225);
         ctx.translate(0, 10);
 
         ctx.fillStyle = "orange";
